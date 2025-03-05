af5839e6d7ef094e9c48288e3f5abff20872433b
Joseph Baruch <josephbaruch48@gmail.com>
2025-03-04 20:19:21 -0800

    Temp irrelivant data

--- frontend/src/pages/home/components/SellMain.tsx
+++ frontend/src/pages/home/components/SellMain.tsx
@@ -1,11 +1,97 @@
+import * as React from 'react';
 import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Chip from '@mui/material/Chip';
 import Grid2 from '@mui/material/Grid2';
 import Typography from '@mui/material/Typography';
 import FormControl from '@mui/material/FormControl';
 import InputAdornment from '@mui/material/InputAdornment';
 import OutlinedInput from '@mui/material/OutlinedInput';
-
+import { styled } from '@mui/material/styles';
 import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+
+const tempProducts = [
+  {
+    img: 'https://picsum.photos/800/450?random=1',
+    tag: 'Product1',
+    title: 'Product # 1',
+    description:
+      'Product Description.',
+  },
+  {
+    img: 'https://picsum.photos/800/450?random=2',
+    tag: 'Product2',
+    title: 'Product # 2',
+    description:
+      'Product Description.',
+  },
+  {
+    img: 'https://picsum.photos/800/450?random=3',
+    tag: 'Product3',
+    title: 'Product # 3',
+    description:
+      'Product Description.',
+  },
+  {
+    img: 'https://picsum.photos/800/450?random=4',
+    tag: 'Product4',
+    title: 'Product # 4',
+    description:
+      'Product Description.',
+  },
+  {
+    img: 'https://picsum.photos/800/450?random=45',
+    tag: 'Product5',
+    title: 'Product # 5',
+    description:
+      'Product Description.',
+  },
+  {
+    img: 'https://picsum.photos/800/450?random=6',
+    tag: 'Product6',
+    title: 'Product # 6,',
+    description:
+      'Product Description.',
+  },
+];
+
+const StyledCard = styled(Card)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  padding: 0,
+  height: '100%',
+  backgroundColor: theme.palette.background.paper,
+  '&:hover': {
+    backgroundColor: 'transparent',
+    cursor: 'pointer',
+  },
+  '&:focus-visible': {
+    outline: '3px solid',
+    outlineColor: 'hsla(210, 98%, 48%, 0.5)',
+    outlineOffset: '2px',
+  },
+}));
+
+const StyledCardContent = styled(CardContent)({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 4,
+  padding: 16,
+  flexGrow: 1,
+  '&:last-child': {
+    paddingBottom: 16,
+  },
+});
+
+const StyledTypography = styled(Typography)({
+  display: '-webkit-box',
+  WebkitBoxOrient: 'vertical',
+  WebkitLineClamp: 2,
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+});
 
 export function Search() {
   return (
@@ -28,16 +114,296 @@ export function Search() {
   );
 }
 
-export default function MainContent() {
+export default function SellMain() {
+  const [focusedCardIndex, setFocusedCardIndex] = React.useState<number | null>(
+    null,
+  );
+
+  const handleFocus = (index: number) => {
+    setFocusedCardIndex(index);
+  };
+
+  const handleBlur = () => {
+    setFocusedCardIndex(null);
+  };
+
+  const handleClick = () => {
+    console.info('You clicked the filter chip.');
+  };
 
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', gap: 4 }}>
+    <Box sx={{ display: 'flex', flexDirection: 'column', gap: 4, width: '100%' }}>
+      {/* Title Section */}
+      <div>
         <Typography variant="h1" gutterBottom>
-            Sell
+          Sell
         </Typography>
-        <Grid2 container spacing={2} columns={12}>
-            <Typography>Coming soon</Typography>
+        <Typography>List your products for sale</Typography>
+      </div>
+      {/* Title Section */}
+      {/* Search Bar/Product Filters */}
+      <Box
+        sx={{
+          display: { xs: 'flex', sm: 'none' },
+          flexDirection: 'row',
+          gap: 1,
+          width: { xs: '100%', md: 'fit-content' },
+          overflow: 'auto',
+        }}
+      >
+        <Search />
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: { xs: 'column-reverse', md: 'row' },
+          width: '100%',
+          justifyContent: 'space-between',
+          alignItems: { xs: 'start', md: 'center' },
+          gap: 4,
+          overflow: 'auto',
+        }}
+      >
+        <Box
+          sx={{
+            display: 'inline-flex',
+            flexDirection: 'row',
+            gap: 3,
+            overflow: 'auto',
+          }}
+        >
+          <Chip onClick={handleClick} size="medium" label="All categories" />
+          <Chip
+            onClick={handleClick}
+            size="medium"
+            label="Stuff"
+            sx={{
+              backgroundColor: 'transparent',
+              border: 'none',
+            }}
+          />
+          <Chip
+            onClick={handleClick}
+            size="medium"
+            label="Things"
+            sx={{
+              backgroundColor: 'transparent',
+              border: 'none',
+            }}
+          />
+        </Box>
+        <Box
+          sx={{
+            display: { xs: 'none', sm: 'flex' },
+            flexDirection: 'row',
+            gap: 1,
+            width: { xs: '100%', md: 'fit-content' },
+            overflow: 'auto',
+          }}
+        >
+          <Search />
+        </Box>
+      </Box>
+      {/* Search Bar/Product Filters */}
+      {/* Products */}
+      <Grid2 container spacing={2} columns={12}>
+        <Grid2 size={{ xs: 12, md: 6 }}>
+          <StyledCard
+            variant="outlined"
+            onFocus={() => handleFocus(0)}
+            onBlur={handleBlur}
+            tabIndex={0}
+            className={focusedCardIndex === 0 ? 'Mui-focused' : ''}
+          >
+            <CardMedia
+              component="img"
+              alt="green iguana"
+              image={tempProducts[0].img}
+              sx={{
+                aspectRatio: '16 / 9',
+                borderBottom: '1px solid',
+                borderColor: 'divider',
+              }}
+            />
+            <StyledCardContent>
+              <Typography gutterBottom variant="caption" component="div">
+                {tempProducts[0].tag}
+              </Typography>
+              <Typography gutterBottom variant="h6" component="div">
+                {tempProducts[0].title}
+              </Typography>
+              <StyledTypography variant="body2" color="text.secondary" gutterBottom>
+                {tempProducts[0].description}
+              </StyledTypography>
+            </StyledCardContent>
+          </StyledCard>
         </Grid2>
+        <Grid2 size={{ xs: 12, md: 6 }}>
+          <StyledCard
+            variant="outlined"
+            onFocus={() => handleFocus(1)}
+            onBlur={handleBlur}
+            tabIndex={0}
+            className={focusedCardIndex === 1 ? 'Mui-focused' : ''}
+          >
+            <CardMedia
+              component="img"
+              alt="green iguana"
+              image={tempProducts[1].img}
+              aspect-ratio="16 / 9"
+              sx={{
+                borderBottom: '1px solid',
+                borderColor: 'divider',
+              }}
+            />
+            <StyledCardContent>
+              <Typography gutterBottom variant="caption" component="div">
+                {tempProducts[1].tag}
+              </Typography>
+              <Typography gutterBottom variant="h6" component="div">
+                {tempProducts[1].title}
+              </Typography>
+              <StyledTypography variant="body2" color="text.secondary" gutterBottom>
+                {tempProducts[1].description}
+                </StyledTypography>
+              </StyledCardContent>
+          </StyledCard>
+        </Grid2>
+        <Grid2 size={{ xs: 12, md: 4 }}>
+          <StyledCard
+            variant="outlined"
+            onFocus={() => handleFocus(2)}
+            onBlur={handleBlur}
+            tabIndex={0}
+            className={focusedCardIndex === 2 ? 'Mui-focused' : ''}
+            sx={{ height: '100%' }}
+          >
+            <CardMedia
+              component="img"
+              alt="green iguana"
+              image={tempProducts[2].img}
+              sx={{
+                height: { sm: 'auto', md: '50%' },
+                aspectRatio: { sm: '16 / 9', md: '' },
+              }}
+            />
+            <StyledCardContent>
+              <Typography gutterBottom variant="caption" component="div">
+                {tempProducts[2].tag}
+              </Typography>
+              <Typography gutterBottom variant="h6" component="div">
+                {tempProducts[2].title}
+              </Typography>
+              <StyledTypography variant="body2" color="text.secondary" gutterBottom>
+                {tempProducts[2].description}
+                </StyledTypography>
+              </StyledCardContent>
+          </StyledCard>
+        </Grid2>
+        <Grid2 size={{ xs: 12, md: 4 }}>
+          <Box
+            sx={{ display: 'flex', flexDirection: 'column', gap: 2, height: '100%' }}
+          >
+            <StyledCard
+              variant="outlined"
+              onFocus={() => handleFocus(3)}
+              onBlur={handleBlur}
+              tabIndex={0}
+              className={focusedCardIndex === 3 ? 'Mui-focused' : ''}
+              sx={{ height: '100%' }}
+            >
+              <StyledCardContent
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'space-between',
+                  height: '100%',
+                }}
+              >
+                <div>
+                  <Typography gutterBottom variant="caption" component="div">
+                    {tempProducts[3].tag}
+                  </Typography>
+                  <Typography gutterBottom variant="h6" component="div">
+                    {tempProducts[3].title}
+                  </Typography>
+                  <StyledTypography
+                    variant="body2"
+                    color="text.secondary"
+                    gutterBottom
+                  >
+                    {tempProducts[3].description}
+                  </StyledTypography>
+                </div>
+              </StyledCardContent>
+            </StyledCard>
+            <StyledCard
+              variant="outlined"
+              onFocus={() => handleFocus(4)}
+              onBlur={handleBlur}
+              tabIndex={0}
+              className={focusedCardIndex === 4 ? 'Mui-focused' : ''}
+              sx={{ height: '100%' }}
+            >
+              <StyledCardContent
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'space-between',
+                  height: '100%',
+                }}
+              >
+                <div>
+                  <Typography gutterBottom variant="caption" component="div">
+                    {tempProducts[4].tag}
+                  </Typography>
+                  <Typography gutterBottom variant="h6" component="div">
+                    {tempProducts[4].title}
+                  </Typography>
+                  <StyledTypography
+                    variant="body2"
+                    color="text.secondary"
+                    gutterBottom
+                  >
+                    {tempProducts[4].description}
+                  </StyledTypography>
+                </div>
+              </StyledCardContent>
+            </StyledCard>
+          </Box>
+        </Grid2>
+        <Grid2 size={{ xs: 12, md: 4 }}>
+          <StyledCard
+            variant="outlined"
+            onFocus={() => handleFocus(5)}
+            onBlur={handleBlur}
+            tabIndex={0}
+            className={focusedCardIndex === 5 ? 'Mui-focused' : ''}
+            sx={{ height: '100%' }}
+          >
+            <CardMedia
+              component="img"
+              alt="green iguana"
+              image={tempProducts[5].img}
+              sx={{
+                height: { sm: 'auto', md: '50%' },
+                aspectRatio: { sm: '16 / 9', md: '' },
+              }}
+            />
+            <StyledCardContent>
+              <Typography gutterBottom variant="caption" component="div">
+                {tempProducts[5].tag}
+              </Typography>
+              <Typography gutterBottom variant="h6" component="div">
+                {tempProducts[5].title}
+              </Typography>
+              <StyledTypography variant="body2" color="text.secondary" gutterBottom>
+                {tempProducts[5].description}
+                </StyledTypography>
+              </StyledCardContent>
+          </StyledCard>
+        </Grid2>
+      </Grid2>
     </Box>
   );
 }
