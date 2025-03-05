38093904ec3d44ea7ffdcf861a4b9617c6a2118e
Joseph Baruch <josephbaruch48@gmail.com>
2025-03-04 20:01:09 -0800

    Cleanup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,4 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import CreateUser from './CreateUser';
-// import CreateProduct from './CreateProduct'
 import './App.css';
 import HomePage from './pages/home/home'
 import SignIn from './pages/login/SignIn';
@@ -12,11 +10,7 @@ function App() {
   return (
     <Router>
       <div>
-        {/* <h1>eCommerce</h1> */}
-        {/* <Link to="/create-user">Create User</Link> */}
         <Routes>
-
-          {/* <Route path="/create-user" element={<CreateUser />} /> */}
           <Route path="/" element={<HomePage/>} />
           <Route path="/signin" element={<SignIn/>} />
           <Route path="/signup" element={<SignUp/>} />
