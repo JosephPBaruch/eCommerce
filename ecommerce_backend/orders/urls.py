dcc3f97881f3565ea80df1305d396b0067bc6b06
Joseph Baruch <josephbaruch48@gmail.com>
2025-04-20 12:35:04 -0700

    more orders endpoints

--- ecommerce_backend/orders/urls.py
+++ ecommerce_backend/orders/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import OrdersViewSet
+from .views import OrdersViewSet, CartOrdersView
 
 urlpatterns = [
     path('', OrdersViewSet.as_view({'get': 'list', 'post': 'create'}), name='order'),
-    path('<uuid:pk>/', OrdersViewSet.as_view({'put': 'update', 'delete': 'destroy'}), name='order-detail'),
-]
+    path('<uuid:pk>/', OrdersViewSet.as_view({'get': 'list', 'put': 'update', 'delete': 'destroy'}), name='order-detail'),
+    path('cart/', CartOrdersView.as_view(), name='cart-orders'),
+    ]
--- ecommerce_backend/orders/views.py
+++ ecommerce_backend/orders/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import Order
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
@@ -16,3 +18,12 @@ class OrdersViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return Order.objects.filter()
+
+class CartOrdersView(APIView):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+    def get(self, request, *args, **kwargs):
+        cart_orders = Order.objects.filter(user=request.user, status='cart')
+        serializer = OrderSerializer(cart_orders, many=True)
+        return Response(serializer.data)
